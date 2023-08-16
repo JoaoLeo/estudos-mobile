b3f8b855de6dc302c71258139c506ea4a1a2889d
João Leonardo <joaoleonardo9921@gmail.com>
2023-08-16 19:56:42 -0300

    teste git2

--- conceitos/App.js
+++ conceitos/App.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'purple',
-    alignItems: 'center',
+    alignItems: 'right',
     justifyContent: 'center',
   },
 });
