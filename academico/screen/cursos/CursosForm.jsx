8ec028d068598f56176bfd4b7e8b98c3895eef4b
João Leonardo <joaoleonardo9921@gmail.com>
2023-10-09 21:43:38 -0300

    aula dia 09-10

--- academico/screen/cursos/CursosForm.jsx
+++ academico/screen/cursos/CursosForm.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ScrollView } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
 
 const CursosForm = () => {
+  
+  const [dados, setDados] = useState({});
+
+  function handleChange(valor, campo){
+    setDados({...dados, [campo]: valor})
+  }
+
+  function salvar(){
+   console.log(dados);
+  }
+  
   return (
     <>
+    <ScrollView style={{
+      marginTop: 5
+    }}> 
+    <Text> Formulário de curso </Text>
+    <TextInput style={{
+      marginTop: 5
+    }} 
+    label="Nome" 
+    mode='outlined' 
+    value={dados.nome}
+    onChangeText={(valor) => handleChange(valor, "nome")}
+    />
+    <TextInput style={{ 
+      marginTop: 5}} 
+      label="Duração" 
+      keyboardType='decimal-pad'
+      mode='outlined' 
+      value={dados.duracao}  
+      onChangeText={(valor) => handleChange(valor, "duracao")}
+      />
+    <TextInput style={{ 
+      marginTop: 5 }} label="Modalidade"
+       mode='outlined' 
+       value={dados.modalidade}
+       onChangeText={(valor) => handleChange(valor, "modalidade")}
+       />
+    </ScrollView>
+    <Button onPress={salvar}> Enviar </Button>
     </>
   )
 }
--- academico/screen/disciplinas/DisciplinasForm.jsx
+++ academico/screen/disciplinas/DisciplinasForm.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
 const DisciplinasForm = () => {
+  const [dados, setDados] = useState({});
+
+  function handleChange(valor, campo){
+    setDados({...dados, [campo]: valor})
+  }
+
+  function salvar(){
+   console.log(dados);
+  }
+  
   return (
     <>
-    
+    <ScrollView style={{
+      marginTop: 5
+    }}> 
+    <Text> Formulário de disciplinas </Text>
+    <TextInput style={{ 
+      marginTop: 5}} 
+      label="Duração" 
+      keyboardType='decimal-pad'
+      mode='outlined' 
+      value={dados.id}  
+      onChangeText={(valor) => handleChange(valor, "id")}
+      />
+    <TextInput style={{
+      marginTop: 5
+    }} 
+    label="Nome" 
+    mode='outlined' 
+    value={dados.nome}
+    onChangeText={(valor) => handleChange(valor, "nome")}
+    />
+
+    <TextInput style={{ 
+      marginTop: 5 }} label="curso_id"
+       mode='outlined' 
+       value={dados.curso_id}
+       onChangeText={(valor) => handleChange(valor, "curso_id")}
+       />
+    </ScrollView>
+    <Button onPress={salvar}> Enviar </Button>
     </>
   )
 }
